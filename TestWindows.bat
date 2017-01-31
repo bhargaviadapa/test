d55102a66732971c80b17621da3a5b7a8a5de79e
Sarang <2500055@CHICOC02QF8PG.local>
2017-01-30 22:38:15 -0600

    Testing

--- TestWindows.bat
+++ TestWindows.bat
@@ -1,4 +1,4 @@
 @echo off
-echo 140.95.206.102 world.hyattstg.com > %temp%\temphosts.txt
+echo 140.95.206.102 goldpassport.hyattstg.com > %temp%\temphosts.txt
 type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
 copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
